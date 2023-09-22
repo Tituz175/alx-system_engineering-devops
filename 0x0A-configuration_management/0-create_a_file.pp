601d1da122b81161603c39e74c390dd0bdc4706c
tituz175 <tobititus01@gmail.com>
2023-09-22 16:36:55 -0600

    task 0 modified

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 
 file { '/tmp/school':
     ensure  => 'file',
-    mode    => 'o744',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet'
